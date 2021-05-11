e895cedb4c98126935174123a4e3e86e18d70ebb
Ales Krasouski <2051020@gmail.com>
2021-05-11 16:43:42 +0300

    Fix issues: - IOS 3.1.80: When pressing logout on the client the app crashes. - Cashier app should go to home page as default after adding stamps or redeems. Now it stays on the same page. It should redirect back to the scan qr-code menu page. - For our production version Jari and I have agreed that our home page in Mobietu should be our Profile page. - This text ”this is a required field” is missing in FI. In FI it should be ”tämä on pakollinen kenttä”

--- components/react-utils/src/utils/replaceVars.ts
+++ components/react-utils/src/utils/replaceVars.ts
@@ -1,5 +1,5 @@
 const replaceVars = (string: string, props: any = {}) => {
-  return string.replace(
+  return string?.replace(
     /%([a-z,A-Z,0-9]*)%/g,
     (_: string, propName: string) => {
       return props[propName];
